ffe38e719a25084f5695423db2bfa3499523e023
AmirMasoud Sheidayi <amirmasood33@gmail.com>
2016-06-24 14:44:23 +0430

    add fancy header for widget title

--- functions.php
+++ functions.php
@@ -93,8 +93,8 @@ function torlangame_widgets_init() {
 		'description'   => esc_html__( 'Add widgets here.', 'torlangame' ),
 		'before_widget' => '<section id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
+		'before_title'  => '<h2 class="widget-title fancy"><span>',
+		'after_title'   => '</span></h2>',
 	) );
 }
 add_action( 'widgets_init', 'torlangame_widgets_init' );
