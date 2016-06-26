e6a0c8c20f34d0b58d90398e91514d5bd0a6d2cc
AmirMasoud Sheidayi <amirmasood33@gmail.com>
2016-06-26 18:13:01 +0430

    add fancy icons to header links

--- header.php
+++ header.php
@@ -31,12 +31,12 @@
 						<h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 						<nav class="links">
 							<ul>
-								<li><a href="http://torlangame.com/category/%D8%AE%D8%A8%D8%B1/">خبر</a></li>
-								<li><a href="http://torlangame.com/category/%D9%88%DB%8C%DA%98%D9%87-%DA%AF%DB%8C%D9%85%D8%B1-%D9%87%D8%A7/">ویژه گیمرها</a></li>
-								<li><a href="http://torlangame.com/category/%D8%B1%D8%A7%D9%87%D9%86%D9%85%D8%A7-%D9%88-%D8%AA%D9%82%D9%84%D8%A8/">راهنما و تقلب</a></li>
-								<li><a href="http://torlangame.com/category/%D8%B3%DB%8C%D8%B3%D8%AA%D9%85-%D9%85%D9%88%D8%B1%D8%AF-%D9%86%DB%8C%D8%A7%D8%B2/">سیستم مورد نیاز</a></li>
-								<li><a href="http://torlangame.com/category/%D8%AF%D8%A7%D9%86%D9%84%D9%88%D8%AF/">دانلود</a></li>
-								<li><a href="http://torlangame.com/movies/">ویدیو</a></li>
+								<li><a href="http://torlangame.com/category/%D8%AE%D8%A8%D8%B1/"><i class="fa fa-newspaper-o" aria-hidden="true"></i> خبر</a></li>
+								<li><a href="http://torlangame.com/category/%D9%88%DB%8C%DA%98%D9%87-%DA%AF%DB%8C%D9%85%D8%B1-%D9%87%D8%A7/"><i class="fa fa-gamepad" aria-hidden="true"></i> ویژه گیمرها</a></li>
+								<li><a href="http://torlangame.com/category/%D8%B1%D8%A7%D9%87%D9%86%D9%85%D8%A7-%D9%88-%D8%AA%D9%82%D9%84%D8%A8/"><i class="fa fa-motorcycle" aria-hidden="true"></i> راهنما و تقلب</a></li>
+								<li><a href="http://torlangame.com/category/%D8%B3%DB%8C%D8%B3%D8%AA%D9%85-%D9%85%D9%88%D8%B1%D8%AF-%D9%86%DB%8C%D8%A7%D8%B2/"><i class="fa fa-laptop" aria-hidden="true"></i> سیستم مورد نیاز</a></li>
+								<li><a href="http://torlangame.com/category/%D8%AF%D8%A7%D9%86%D9%84%D9%88%D8%AF/"><i class="fa fa-cloud-download" aria-hidden="true"></i> دانلود</a></li>
+								<li><a href="http://torlangame.com/movies/"><i class="fa fa-video-camera" aria-hidden="true"></i> ویدیو</a></li>
 							</ul>
 						</nav>
 						<nav class="main">
@@ -69,32 +69,32 @@
 								<ul class="links">
 									<li>
 										<a href="http://torlangame.com/category/%D8%AE%D8%A8%D8%B1/">
-											<h3>خبر</h3>
+											<h3><i class="fa fa-newspaper-o" aria-hidden="true"></i> خبر</h3>
 										</a>
 									</li>
 									<li>
 										<a href="http://torlangame.com/category/%D9%88%DB%8C%DA%98%D9%87-%DA%AF%DB%8C%D9%85%D8%B1-%D9%87%D8%A7/">
-											<h3>ویژه گیمرها</h3>
+											<h3><i class="fa fa-gamepad" aria-hidden="true"></i> ویژه گیمرها</h3>
 										</a>
 									</li>
 									<li>
 										<a href="http://torlangame.com/category/%D8%B1%D8%A7%D9%87%D9%86%D9%85%D8%A7-%D9%88-%D8%AA%D9%82%D9%84%D8%A8/">
-											<h3>راهنما و تقلب</h3>
+											<h3><i class="fa fa-motorcycle" aria-hidden="true"></i> راهنما و تقلب</h3>
 										</a>
 									</li>
 									<li>
 										<a href="http://torlangame.com/category/%D8%B3%DB%8C%D8%B3%D8%AA%D9%85-%D9%85%D9%88%D8%B1%D8%AF-%D9%86%DB%8C%D8%A7%D8%B2/">
-											<h3>سیستم مورد نیاز</h3>
+											<h3><i class="fa fa-laptop" aria-hidden="true"></i> سیستم مورد نیاز</h3>
 										</a>
 									</li>
 									<li>
 										<a href="http://torlangame.com/category/%D8%AF%D8%A7%D9%86%D9%84%D9%88%D8%AF/">
-											<h3>دانلود</h3>
+											<h3><i class="fa fa-cloud-download" aria-hidden="true"></i> دانلود</h3>
 										</a>
 									</li>
 									<li>
 										<a href="http://torlangame.com/movies/">
-											<h3>ویدیو</h3>
+											<h3><i class="fa fa-video-camera" aria-hidden="true"></i> ویدیو</h3>
 										</a>
 									</li>
 								</ul>
