b68bde5d72d4248b0f9ff8430d9389d815de15f6
AmirMasoud Sheidayi <amirmasood33@gmail.com>
2016-07-15 17:31:14 +0430

    add two cats to the home

--- home.php
+++ home.php
@@ -1,25 +1,38 @@
 <?php
 get_header(); ?>
 	<!-- Main -->
-	<div id="main" class="row">
-		<div class="col-md-9 col-xs-12">
-			<?php echo do_shortcode("[metaslider id=3508]"); ?>
+	<div id="main">
+		<div class="row">
+			<div class="col-md-9 col-xs-12 margin-top-20">
+				<?php echo do_shortcode("[metaslider id=3508]"); ?>
 
-			<div class="row">
-				<div class="col-md-6 col-xs-12 margin-top-20">
-					<img src="https://torlangame.com/wp-content/uploads/2016/07/aprat-torlan-466-95.png" class="img-responsive center-block" width="100%" alt="telegram channel">
+				<div class="row">
+					<div class="col-md-6 col-xs-12 margin-top-20">
+						<img src="https://torlangame.com/wp-content/uploads/2016/07/aprat-torlan-466-95.png" class="img-responsive center-block" width="100%" alt="telegram channel">
+					</div>
+
+					<div class="col-md-6 col-xs-12 margin-top-20">
+						<img src="https://torlangame.com/wp-content/uploads/2016/07/telegram-torlan-466-95.png" class="img-responsive center-block" width="100%" alt="telegram channel">
+					</div>
 				</div>
 
-				<div class="col-md-6 col-xs-12 margin-top-20">
-					<img src="https://torlangame.com/wp-content/uploads/2016/07/telegram-torlan-466-95.png" class="img-responsive center-block" width="100%" alt="telegram channel">
+				<div class="row">
+					<div class="col-md-6 col-xs-12 margin-top-20">
+						<h2 class="widget-title fancy"><span><a href="<?php echo get_category_link( get_cat_ID( 'راهنما و تقلب' ) ) ?>">آخرین راهنما و تقلب ها</a></span></h2>
+						<?php the_widget( 'torlangame_Widget_Recent_Posts', array( 'number' => 5, 'category' => get_cat_ID( 'راهنما و تقلب' ) ) ) ?>
+					</div>
+					<div class="col-md-6 col-xs-12 margin-top-20">
+						<h2 class="widget-title fancy"><span><a href="<?php echo get_category_link( get_cat_ID( 'خبر' ) ) ?>">آخرین خبر ها</a></span></h2>
+						<?php the_widget( 'torlangame_Widget_Recent_Posts', array( 'number' => 5, 'category' => get_cat_ID( 'خبر' ) ) ) ?>
+					</div>
 				</div>
-			</div>
-		</div>
 
-		<div class="col-md-3 col-xs-12 margin-top-20">
-			<h2 class="widget-title fancy"><span><a href="<?php echo get_post_type_archive_link( 'movies' ) ?>">آخرین ویدیو ها</a></span></h2>
-			<?php the_widget( 'torlangame_Widget_Recent_Movies', 'number=2' ) ?>
-		</div>
-	</div>
+			</div><!-- col-md-9 col-xs-12 margin-top-20 -->
+			<div class="col-md-3 col-xs-12 margin-top-20">
+				<h2 class="widget-title fancy"><span><a href="<?php echo get_post_type_archive_link( 'movies' ) ?>">آخرین ویدیو ها</a></span></h2>
+				<?php the_widget( 'torlangame_Widget_Recent_Movies', 'number=2' ) ?>
+			</div><!-- col-md-3 col-xs-12 margin-top-20 -->
+		</div><!-- .row -->
+	</div><!-- #main -->
 <?php
 get_footer();
